150032bd853aff52d929e4ce1e9daaccda1d546c
Denis Solovets <denis.solovets@dataart.com>
2020-05-21 20:04:39 +0300

    [gist] Prepend gist with some data

---
+++ dev_scripts/gist.js
@@ -0,0 +1,37 @@
+const concat = require('concat-files');
+const path = require('path');
+const fs = require('fs');
+
+const pkg = require('../package');
+const helpers = require('./scss-helpers');
+const functions = require('./scss-functions');
+
+const files = ['./dev_scripts/_gist.scss'];
+
+for (let i = 0; i < helpers.length; i++) {
+    files.push('./helpers/_' + helpers[i] + '.scss');
+}
+
+for (let i = 0; i < functions.length; i++) {
+    files.push('./functions/_' + functions[i] + '.scss');
+}
+
+const gistPath = path.join(process.cwd(), '../str.scss.gist/_str.scss');
+
+const gistIntro = '// ' + pkg.name + ' | v. ' + pkg.version + ' | ' + pkg.license + ' license\n' +
+    '// ' + pkg.description + '\n' +
+    '//\n' +
+    '// Homepage: ' + pkg.homepage + '\n' +
+    '// Report an issue: ' + pkg.bugs.url + '\n' +
+    '// Author: ' + pkg.author + '\n' +
+    '// This file is always up to date.\n\n';
+
+fs.writeFile('./dev_scripts/_gist.scss', gistIntro, function (error) {
+    if (error) throw error;
+    concat(files, gistPath, function(err) {
+        if (err) throw err;
+        console.log('Gist updated');
+    });
+});
+
+
